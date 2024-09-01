3365a546f027c70f19317197704550351261dfe9
Wasif <islamwasif3@gmail.com>
2024-09-01 18:32:08 -0400

    feat: add kepler to eci cartesian function to generate orbits in cartesian coordinates

--- HW1/hw1_main.m
+++ HW1/hw1_main.m
@@ -3,7 +3,15 @@
 % Date: Aug 31st, 2024
 
 %% Initialization
+clear;
+clc;
 
 % Import Necessary Constants
 addpath(genpath('..'));   % Add homework parent directory to import shared functions
+global G MU_EARTH;
 load("standard_gravitation_parameter.mat");
+
+%% Problem 1
+
+% Define Orbits
+
