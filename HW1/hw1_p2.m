64b08706f16a212745dccd6144706b447d2a3a39
Wasif <islamwasif3@gmail.com>
2024-09-05 19:46:46 -0400

    solutions to problem 2a

--- HW1/hw1_p2.m
+++ HW1/hw1_p2.m
@@ -31,6 +31,7 @@ station_data.StationR_ECEF = norm([station_data.StationX_ECEF(1), ...
 station_data.SATX_ECI = coordinate_data.SatelliteXECI_Km_;
 station_data.SATY_ECI = coordinate_data.SatelliteYECI_Km_;
 station_data.SATZ_ECI = coordinate_data.SatelliteZECI_Km_;
+station_data.SAT_RANGE = coordinate_data.SatelliteRange_Km_;
 
 % Define Cell Arrays for ECI XYZ Coords for Station and Satellite
 sat_eci = cell(length(station_data.StationX_ECEF), 1);
@@ -47,7 +48,7 @@ for sample_length = 1:length(station_data.StationX_ECEF)
     stat_eci{sample_length} = stat_XYZ;
 end
 
-%% Problem 2
+%% Problem 2a
 
 % Calculate Local and Greenwich Mean Sidereal Time
 [GMST, LMST] = mjd2sidereal(station_data.MJD, station_data.GEOC_LONG);
@@ -56,6 +57,8 @@ end
 R = station_data.StationR_ECEF;
 
 stat_eci_calc = cell(length(LMST), 1);     % Pre-allocating Matrix Array
+stat_eci_error = zeros(length(LMST), 1);   % Pre-allocating error term array
+
 for matrix_index = 1:length(LMST)
 
     % Acquire ECI Coordinates for Station
@@ -65,25 +68,39 @@ for matrix_index = 1:length(LMST)
 
     stat_eci_calc{matrix_index} = RT;
 
+    % Calculate error between derived and actual
+    stat_eci_error(matrix_index) = norm(RT - stat_eci{matrix_index});
+
 end
 
-% Compare ECI Values between Given and Calculated
-
 % Calculate Range
+sat_range_calc = zeros(length(LMST), 1);
+r_topo = cell(length(LMST), 1);               % eq. 2.28 in the script
+for sample_index = 1:length(LMST)
+    dist_array = sat_eci{sample_index} - stat_eci_calc{sample_index};
+    range = norm(dist_array);
+    sat_range_calc(sample_index) = range;
+    r_topo{sample_index} = dist_array;
+end
+
+% Calculate Error for Range
+sat_range_error = sat_range_calc - station_data.SAT_RANGE;
 
 % Subtract Rtopo from total r, divide by range, get l-hat
+topo_dec = zeros(length(LMST), 1);
+for sample_index = 1:length(LMST)
+    r_topo_vec = r_topo{sample_index};
+    topo_dec(sample_index) = rad2deg(asin(r_topo_vec(3) / sat_range_calc(sample_index)));
+end
 
 % solve for delta, raan
 
+% Use Equation 2.37 to solve for azimuth and elevation
 
-% Test
-%X = coordinate_data.StationXECEF_Km_(1);
-%Y = coordinate_data.StationYECEF_Km_(1);
-%Z = coordinate_data.StationZECEF_Km_(1);
+% Generate Error Graphs
 
-%R = norm([X, Y, Z]);
-%R3_Rot = [cos(LMST) -sin(LMST) 0; sin(LMST) cos(LMST) 0; 0 0 1];
-%RT = [R * cos(deg2rad(coordinate_data.StationGeocentricLatitude_deg_(1))) * cos(LMST); ...
-%      R * cos(deg2rad(coordinate_data.StationGeocentricLatitude_deg_(1))) * sin(LMST); ...
-%      R * sin(deg2rad(coordinate_data.StationGeocentricLatitude_deg_(1)))];
+%% Problem 2b
+
+%% Problem 2d
+
 
