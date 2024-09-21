dca29f8aa99e3338262225ef31d5b443ffbe975e
Wasif <islamwasif3@gmail.com>
2024-09-21 03:46:12 -0400

    feat: Implement gauss IOD and finish orbit generation for HW3

--- HW3/hw3_p2.m
+++ HW3/hw3_p2.m
@@ -16,26 +16,166 @@ load("constants.mat");
 ISS_OBS_1.obs_t1 = [-5558.082269000000; 176.170673000000; -3909.745697000000];
 ISS_OBS_1.obs_t2 = [-6077.020846075970; -1187.526562175400; -2801.059165642000];
 ISS_OBS_1.obs_t3 = [-6154.069043656380; -2464.846712251270; -1488.14264248893];
+ISS_OBS_1.v2 = [-1.25746818075924; -5.56989208874611; 5.10696813683801];
 ISS_OBS_1.t1 = datetime('2024-09-18T15:07:01.900');
 ISS_OBS_1.t2 = datetime('2024-09-18T15:11:01.900');
 ISS_OBS_1.t3 = datetime('2024-09-18T15:15:01.900');
 
 % X, Y, Z in Km - Obs 2
-ISS_OBS_2.obs_t1 = [3602.807338947890; -2578.956715773420; 5146.335993511110]; 
-ISS_OBS_2.obs_t2 = [4819.675844045330; -1320.910653608590; 4601.454936466590];
-ISS_OBS_2.obs_t3 = [5686.365561077790; 33.107790076274; 3721.296031977380];
-ISS_OBS_2.t1 = datetime('2024-09-18T17:19:01.900');
-ISS_OBS_2.t2 = datetime('2024-09-18T17:23:01.900');
-ISS_OBS_2.t3 = datetime('2024-09-18T17:27:01.900');
+ISS_OBS_2.obs_t1 = [-5936.711093571230; -3294.639088057420; -216.673358957611]; 
+ISS_OBS_2.obs_t2 = [-5210.246686742630; -4183.505595818840; 1214.134853256750];
+ISS_OBS_2.obs_t3 = [-4103.785845413550; -4767.250417725080; 2556.134001207910];
+ISS_OBS_2.v2 = [3.86564448499101; -3.10582295923938; 5.84815562473533];
+ISS_OBS_2.t1 = datetime('2024-09-18T23:03:01.900');
+ISS_OBS_2.t2 = datetime('2024-09-18T23:07:01.900');
+ISS_OBS_2.t3 = datetime('2024-09-18T23:11:01.900');
 
 % Change MU Constants to be in Km
 MU_EARTH = MU_EARTH * 1e-9;
 
 % Change Elevation Units
-Purdue.STATION_ALT = Purdue.STATION_ALT * 1e-3;
+STAT_ECEF = GDLL2ECEF(Purdue.GD_LAT, Purdue.GD_LONG, Purdue.STATION_ALT) * 1e-3;
+
+% Topcenter ECI - OBS 1
+O1_R1 = ecef2eci(ISS_OBS_1.t1, STAT_ECEF);
+O1_R2 = ecef2eci(ISS_OBS_1.t2, STAT_ECEF);
+O1_R3 = ecef2eci(ISS_OBS_1.t3, STAT_ECEF);
+
+% Topocenter ECI - OBS 2
+O2_R1 = ecef2eci(ISS_OBS_2.t1, STAT_ECEF);
+O2_R2 = ecef2eci(ISS_OBS_2.t2, STAT_ECEF);
+O2_R3 = ecef2eci(ISS_OBS_2.t3, STAT_ECEF);
 
 % Generate IOD - RA, Dec Observation
 
+% OBS 1
+[ra1_1, dec1_1] = ECI2DEC_RA(ISS_OBS_1.obs_t1, O1_R1);
+[ra1_2, dec1_2] = ECI2DEC_RA(ISS_OBS_1.obs_t2, O1_R2);
+[ra1_3, dec1_3] = ECI2DEC_RA(ISS_OBS_1.obs_t3, O1_R3);
+
+% OBS 2
+[ra2_1, dec2_1] = ECI2DEC_RA(ISS_OBS_2.obs_t1, O2_R1);
+[ra2_2, dec2_2] = ECI2DEC_RA(ISS_OBS_2.obs_t2, O2_R2);
+[ra2_3, dec2_3] = ECI2DEC_RA(ISS_OBS_2.obs_t3, O2_R3);
+
+% Assign Result to Observation Struct
+obs_ra1.o1.RA = ra1_1;
+obs_ra1.o2.RA = ra1_2;
+obs_ra1.o3.RA = ra1_3;
+
+obs_ra1.o1.DEC = dec1_1;
+obs_ra1.o2.DEC = dec1_2;
+obs_ra1.o3.DEC = dec1_3;
+
+obs_ra2.o1.RA = ra2_1;
+obs_ra2.o2.RA = ra2_2;
+obs_ra2.o3.RA = ra2_3;
+
+obs_ra2.o1.DEC = dec2_1;
+obs_ra2.o2.DEC = dec2_2;
+obs_ra2.o3.DEC = dec2_3;
+
+[r2_obs1_gauss, v2_obs1_gauss] = gauss_v2_iod(obs_ra1.o1, obs_ra1.o2, obs_ra1.o3, ...
+    ISS_OBS_1.t1, ISS_OBS_1.t2, ISS_OBS_1.t3, STAT_ECEF, MU_EARTH);
+
+[r2_obs2_gauss, v2_obs2_gauss] = gauss_v2_iod(obs_ra2.o1, obs_ra2.o2, obs_ra2.o3, ...
+    ISS_OBS_2.t1, ISS_OBS_2.t2, ISS_OBS_2.t3, STAT_ECEF, MU_EARTH);
+
 % Generate IOD - Range Observation
+v2_obs1_gibbs = gibbs_v2_iod(ISS_OBS_1.obs_t1, ISS_OBS_1.obs_t2, ISS_OBS_1.obs_t3, MU_EARTH);
+v2_obs2_gibbs = gibbs_v2_iod(ISS_OBS_2.obs_t1, ISS_OBS_2.obs_t2, ISS_OBS_2.obs_t3, MU_EARTH);
+
+% Generate Orbit from Original RV2 - OBS1
+orb_o1_orig = KeplerOrbitSimple();
+orb_o1_orig.mu = MU_EARTH;
+orb_o1_orig.t0 = 0.0;
+orb_o1_orig.tend = 86400;
+orb_o1_orig.dt = 15.0;
+orb_o1_orig.x0 = [ISS_OBS_1.obs_t2; ISS_OBS_1.v2];
+[tx_o1_orig, xn_o1_orig] = orb_o1_orig.propagate_simple_kepler();
+
+% Generate Orbit from Original RV2 - OBS2
+orb_o2_orig = KeplerOrbitSimple();
+orb_o2_orig.mu = MU_EARTH;
+orb_o2_orig.t0 = 0.0;
+orb_o2_orig.tend = 86400;
+orb_o2_orig.dt = 15.0;
+orb_o2_orig.x0 = [ISS_OBS_2.obs_t2; ISS_OBS_2.v2];
+[tx_o2_orig, xn_o2_orig] = orb_o1_orig.propagate_simple_kepler();
+
+% Generate Orbit from IOD RV2 - Optical - OBS1
+orb_o1_gauss = KeplerOrbitSimple();
+orb_o1_gauss.mu = MU_EARTH;
+orb_o1_gauss.t0 = 0.0;
+orb_o1_gauss.tend = 86400;
+orb_o1_gauss.dt = 15.0;
+orb_o1_gauss.x0 = [r2_obs1_gauss; v2_obs1_gauss];
+[tx_o1_gauss, xn_o1_gauss] = orb_o1_gauss.propagate_simple_kepler();
+
+% Generate Orbit from IOD RV2 - Optical - OBS2
+orb_o2_gauss = KeplerOrbitSimple();
+orb_o2_gauss.mu = MU_EARTH;
+orb_o2_gauss.t0 = 0.0;
+orb_o2_gauss.tend = 86400;
+orb_o2_gauss.dt = 15.0;
+orb_o2_gauss.x0 = [r2_obs2_gauss; v2_obs2_gauss];
+[tx_o2_gauss, xn_o2_gauss] = orb_o2_gauss.propagate_simple_kepler();
+
+% Generate Orbit from IOD RV2 - Range - OBS1
+orb_o1_gibbs = KeplerOrbitSimple();
+orb_o1_gibbs.mu = MU_EARTH;
+orb_o1_gibbs.t0 = 0.0;
+orb_o1_gibbs.tend = 86400;
+orb_o1_gibbs.dt = 15.0;
+orb_o1_gibbs.x0 = [ISS_OBS_1.obs_t2; v2_obs1_gibbs];
+[tx_o1_gibbs, xn_o1_gibbs] = orb_o1_gibbs.propagate_simple_kepler();
+
+% Generate Orbit from IOD RV2 - Range - OBS2
+orb_o2_gibbs = KeplerOrbitSimple();
+orb_o2_gibbs.mu = MU_EARTH;
+orb_o2_gibbs.t0 = 0.0;
+orb_o2_gibbs.tend = 86400;
+orb_o2_gibbs.dt = 15.0;
+orb_o2_gibbs.x0 = [ISS_OBS_2.obs_t2; v2_obs2_gibbs];
+[tx_o2_gibbs, xn_o2_gibbs] = orb_o2_gibbs.propagate_simple_kepler();
+
+% Plot Results
+
+% Plot Spherical (Representing Earth)
+earth_radius = 6378;        % DIM: [km]
+[x_earth, y_earth, z_earth] = sphere;
+x_earth = x_earth * earth_radius;
+y_earth = y_earth * earth_radius;
+z_earth = z_earth * earth_radius;
+
+figure(1)
+plot3(xn_o1_orig(:,1), xn_o1_orig(:,2), xn_o1_orig(:,3), 'DisplayName', 'Original')
+hold on
+plot3(xn_o1_gauss(:,1), xn_o1_gauss(:,2), xn_o1_gauss(:,3), 'DisplayName', 'Gauss')
+plot3(xn_o1_gibbs(:,1), xn_o1_gibbs(:,2), xn_o1_gibbs(:,3), 'DisplayName', 'Gibbs')
+surf(x_earth, y_earth, z_earth, 'FaceColor', 'k', 'DisplayName', 'Earth');
+axis equal;
+hold off
+grid on
+legend()
+title("Observation 1: Original vs. Gibbs vs Gauss")
+xlabel("R1 [km]")
+ylabel("R2 [km]")
+zlabel("R3 [km]")
+
+figure(2)
+plot3(xn_o2_orig(:,1), xn_o2_orig(:,2), xn_o2_orig(:,3), 'DisplayName', 'Original')
+hold on
+plot3(xn_o2_gauss(:,1), xn_o2_gauss(:,2), xn_o2_gauss(:,3), 'DisplayName', 'Gauss')
+plot3(xn_o2_gibbs(:,1), xn_o2_gibbs(:,2), xn_o2_gibbs(:,3), 'DisplayName', 'Gibbs')
+surf(x_earth, y_earth, z_earth, 'FaceColor', 'k', 'DisplayName', 'Earth');
+axis equal;
+hold off
+grid on
+legend()
+title("Observation 2: Original vs. Gibbs vs Gauss")
+xlabel("R1 [km]")
+ylabel("R2 [km]")
+zlabel("R3 [km]")
 
 
--- shared/utils/iod/gauss_v2_iod.m
+++ shared/utils/iod/gauss_v2_iod.m
@@ -5,13 +5,14 @@
 % Inputs:
 %   [o1, o2 o3]: RA, Dec in degrees for each observation
 %   [t1, t2, t3]: Times of the observations (UTC)
-%   station: Station Struct of Geodedic Information (Lat, Long, Alt)
+%   STATION_ECEF: ECEF vector of observation station
+%   mu: Planet graviational constant
 %
 % Outputs:
 %   v2: Velocity vector accompanying r2
 %
 
-function [v2] = gauss_v2_iod(o1, o2, o3, t1, t2, t3, STATION_ECEF)
+function [r2, v2] = gauss_v2_iod(o1, o2, o3, t1, t2, t3, STATION_ECEF, mu)
     
     % Calculate Station in J2000
     R1 = ecef2eci(t1, STATION_ECEF);
@@ -69,14 +70,51 @@ function [v2] = gauss_v2_iod(o1, o2, o3, t1, t2, t3, STATION_ECEF)
 
     % Calculate Polynomial Coefficients
     a = -A^2 - 2*A*(dot(LH2, R2)) - norm(R2)^2;
-    b
-    c
+    b = -2 * mu * B * (A + dot(LH2, R2));
+    c = -mu^2 * B^2;
 
     % Calculate Roots for R2
+    roots_eq = [1, 0, a, 0, 0, b, 0, 0, c];
+    roots_sol = roots(roots_eq);
+    
+    r2_norm = 0;    % Pre-allocate r2 norm
+    for solution = 1:length(roots_sol)
+        sol = roots_sol(solution);
+
+        % Check for Real Positive Roots
+        if isreal(sol) && (sol > 0)
+
+            % Check for Size
+            if (sol > norm(R1))
+                r2_norm = sol;
+            end
+        end
+    end
+
+    % Return with bad v2 to signal impossible orbit
+    if ~r2_norm
+        v2 = -1;
+        return;
+    end
 
     % Calculate Slant Range
+    p1 = (1 / d0) * (((6 * (d31 * (tau1/tau3) + d21 * (tau13/tau3)) * r2_norm^3 ...
+        + mu * d31 * (tau13^2 - tau1^2) * (tau1/tau3)) / (6 * r2_norm^3 ...
+        + mu * (tau13^2 - tau3^2))) - d11);
 
-    % Calculate Langrange Coefficients
+    p2 = A + (mu * B * (r2_norm^-3));
+
+    p3 = (1 / d0) * (((6 * (d13 * (tau3/tau1) - d23 * (tau13/tau1)) * r2_norm^3 ...
+        + mu * d13 * (tau13^2 - tau3^2) * (tau3/tau1)) / (6 * r2_norm^3 ...
+        + mu * (tau13^2 - tau1^2))) - d33);
+
+    % Calculate Satellite ECI Vector
+    r1 = R1 + (p1 * LH1);
+    r2 = R2 + (p2 * LH2);
+    r3 = R3 + (p3 * LH3);
+
+    % Calculate v2 Using Gibbs
+    v2 = gibbs_v2_iod(r1, r2, r3, mu);
 
 end
 
