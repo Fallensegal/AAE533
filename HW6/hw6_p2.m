c7e070af40e911b47b018f2bddc19428a87dbde1
Wasif <islamwasif3@gmail.com>
2024-10-17 23:45:38 -0400

    start of problem 2

---
+++ HW6/hw6_p2.m
@@ -0,0 +1,14 @@
+% AAE 533 Homework 6
+% Author: Wasif Islam
+% Date: Oct 17th, 2024
+
+%% Initialization
+clear;
+clc;
+
+% Import Constants
+addpath(genpath('..'));   % Add homework parent directory to import shared functions
+load("constants.mat");
+
+%% Problem 2
+
