62b339d9c59db2e137db5c5fa4f9527bbffa79aa
Wasif Islam <islamwasif3@gmail.com>
2024-11-13 18:10:20 -0500

    continue to work on p4

--- HW9/hw9_p4.m
+++ HW9/hw9_p4.m
@@ -24,3 +24,29 @@ GOES.MM = 0.99230085121663;
 % Calculate Semi-Major Axis from Mean Motion
 motion_rads = GOES.MM * 2 * pi / 86400;
 GOES.sma = (MU_EARTH / (motion_rads^2))^(1/3);
+
+% Initial State ECI
+GOES_ECI = kepler_to_eci_cartesian(GOES.sma, GOES.inc, GOES.RAAN, GOES.TA, GOES.argp, GOES.ecc, MU_EARTH);
+orbital_period = (86400/(GOES.MM));
+
+% Propagate 2-Body Orbit
+t0 = 0;
+tend = orbital_period * 2;
+dt = 15;
+tspan = t0:dt:tend;
+options = odeset('RelTol',1e-9,'AbsTol',1e-12);
+x0 = GOES_ECI;
+
+[t1, StateA] = ode45(@(t, r_state) simple_kepler_orbit_pde(r_state, MU_EARTH), tspan, x0, options);
+
+% Propagate SGP4 Orbit
+tspan_sgp4 = tspan ./ 60;
+for i = 1:length(tspan_sgp4)
+    [~,r,~] = sgp4(GEO_INIT, tsince);
+
+            % Convert to ECI Reference Frame
+            precession_matrix = calc_precession(JD_TT);
+            nutation_matrix = calc_nutation(JD_TT);
+            r_J2000 = precession_matrix' * nutation_matrix' * r';
+
+            tle_dict_new = insert(tle_dict_new, sat_name, {r_J2000});
