4cf0977e7aeba3c39b73f1e19365f9edaaf24395
Wasif <islamwasif3@gmail.com>
2024-11-14 14:30:10 -0500

    chore: start p1

---
+++ HW9/hw9_p1.m
@@ -0,0 +1,13 @@
+% AAE 533 Homework 9
+% Author: Wasif Islam
+% Date: November 14th, 2024
+
+%% Initialization
+clear;
+clc;
+
+% Import Constants
+addpath(genpath('..'));
+load("constants.mat");
+
+%% Problem 1
